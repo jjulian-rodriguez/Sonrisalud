b2b43cd19bcfeed8961e26f6bfffef8288640380
jjjuliannn <josejulian2006rm@gmail.com>
2025-08-13 12:31:34 -0600

    Modificando navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import styles from "./navbar.module.css"
 import {useState } from 'react'
+import Logo from '../../assets/logo.png'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMessage, faMobile, faPhone} from "@fortawesome/free-solid-svg-icons";
 
 
 function Navbar(){
@@ -12,10 +15,15 @@ function Navbar(){
 
     return(
         <nav className={styles.navbar}>
-        <button className={styles.hamburger} onClick={openNB}> {open ? "✕" : "☰"}</button>
+            <div className={styles.div}>
+                <img src={Logo} alt="Logo Sonrisalud"  className={styles.logo} />
+                <a className={styles.tel} href="tel:+50600000000"><FontAwesomeIcon icon={faPhone} ></FontAwesomeIcon>0000-0000</a>
+                <a className={styles.cel} href="tel:+50600000000"><FontAwesomeIcon icon={faMessage}></FontAwesomeIcon> 0000-0000</a>
+            </div>
+            <button className={styles.hamburger} onClick={openNB}> {open ? "✕" : "☰"}</button>
             <ul className={styles.navbar_ul}> 
                 <li className={`${styles.navbar_li} ${open ? styles.displayblock : styles.displaynone}`}>
-                    <Link className={`${styles.navbar_link} ${open ? styles.event : styles.nonevent}`} to="/about">¿Quienes Somos?</Link>
+                    <Link className={`${styles.navbar_link} ${open ? styles.event : styles.nonevent}`} to="/about">Nosotros</Link>
                 </li >
                 <li className={`${styles.navbar_li} ${open ? styles.displayblock : styles.displaynone}`}>
                     <Link className={styles.navbar_link} to="">Agendá tu cita</Link>
