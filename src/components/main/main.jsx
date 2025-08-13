bcaf7f1bcc11660cc96388d2a96445e70b18cfc9
jjjuliannn <josejulian2006rm@gmail.com>
2025-08-12 20:37:11 -0600

    Creando parte del main

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,27 +1,41 @@
 import styles from './main.module.css'
-
+import { Link } from 'react-router-dom';
 
 
 function Main(){
     return(
-    <section className={styles.section_info}>
-        <div>
-            <div className={styles.div_info}>
-                <div className={styles.div_contain_info}>
-                </div>
-            </div>
+    <main>
+        <section className={styles.section_services}>
+                <div className={styles.div_services}>
+                    <h2 className={styles.h2}>Nuestros servicios mas solicitados!</h2>
+                    <div className={styles.div_contain_service}>
+                        <Link className={styles.card_service}>
+                            <h3 className={styles.h3}>Estética Dental</h3>
+                        </Link>
 
-            <div className={styles.div_info}>
-                <div className={styles.div_contain_info}>
-                </div>
-            </div>
+                        <Link className={styles.card_service}>
+                            <h3 className={styles.h3}>Ortodoncia</h3>
+                        </Link>
 
-            <div className={styles.div_info}>
-                <div className={styles.div_contain_info}>
+                        <Link className={styles.card_service}>
+                            <h3 className={styles.h3}>Blanqueamiento Dental</h3>
+                        </Link>
+
+                        <Link className={styles.card_service}>
+                            <h3 className={styles.h3}>Ortodonpediatría</h3>
+                        </Link>
+
+                        <Link className={styles.card_service}>
+                            <h3 className={styles.h3}>Implantes Dentales</h3>
+                        </Link>
+
+                        <Link className={styles.card_service}>
+                            <h3 className={styles.h3}>Limpieza Dental</h3>
+                        </Link>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </section>
+        </section>
+    </main>
     );
 }
 
