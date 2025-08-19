91eb223a3bdfe645d1093f1f4e28711e1a4cadca
jjjuliannn <josejulian2006rm@gmail.com>
2025-08-18 22:59:01 -0600

    Formulario de citas terminado y parte avanzada de los especialistas

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -6,38 +6,36 @@ import BlanqueamientoDental from '../../assets/icons/blanqueamiento-dental-icon.
 import Ortodonpediatria from '../../assets/icons/ortodonpediatria-icon.png'
 import ImplanteDental from '../../assets/icons/implantes-dentales-icon.png'
 import LimpiezaDental from '../../assets/icons/limpieza-dental-icon.png'
+import Specialists from './specialists';
 
 
 function Main(){
 
 
 
-    const Services = ({icon, service, description}) => {
+    const Services = ({icon, service}) => {
         return(
         <Link className={styles.card_service}>
                 <img src={icon} alt="Icono" className={styles.icon} />
                 <h3 className={styles.h3}>{service}</h3>
-                <p className={styles.p_description_service}>{description}</p>
-                <button className={styles.btn_masinfo}>Más Información</button>
         </Link>
         );
     }
 
     return(
     <main>
-        <section className={styles.section_info}>
-        </section>
+        <Specialists />
 
         <section className={styles.section_services}>
                 <div className={styles.div_services}>
                     <h2 className={styles.h2}>Nuestros servicios mas solicitados!</h2>
                     <div className={styles.div_contain_service}>
-                        <Services icon={EsteticaDental} service={'Estetica Dental'} description={'Tratamientos para una sonrisa perfecta y armoniosa.'}/>
-                        <Services icon={Ortodoncia} service={'Ortodoncia'} description={'Corrige la posición de los dientes y mejora la mordida.'}/>
-                        <Services icon={BlanqueamientoDental} service={'Blanqueamiento Dental'} description={'Elimina manchas y aclara el tono dental.'}/>
-                        <Services icon={Ortodonpediatria} service={'Ortodonpediatría'} description={'Tratamiento de ortodoncia temprana para niños.'}/>
-                        <Services icon={ImplanteDental} service={'Implantes Dentales'} description={'Solución fija y natural para dientes perdidos.'}/>
-                        <Services icon={LimpiezaDental} service={'Limpieza Dental'} description={'Eliminación profesional de placa y sarro acumulado.'}/>
+                        <Services icon={EsteticaDental} service={'Estetica Dental'} />
+                        <Services icon={Ortodoncia} service={'Ortodoncia'} />
+                        <Services icon={BlanqueamientoDental} service={'Blanqueamiento Dental'} />
+                        <Services icon={Ortodonpediatria} service={'Ortodonpediatría'} />
+                        <Services icon={ImplanteDental} service={'Implantes Dentales'} />
+                        <Services icon={LimpiezaDental} service={'Limpieza Dental'} />
                     </div>
                 </div>
         </section>
